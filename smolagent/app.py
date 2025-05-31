11a866f40b7afcdb21ecd904685275e18a9429c2
MELIGA Vincent <699580621meliga@gmail.com>
2025-05-31 09:08:39 +0100

    fist small agent agest

--- smolagent/app.py
+++ smolagent/app.py
@@ -1,10 +1,10 @@
 from smolagents import CodeAgent, InferenceClientModel
-from tools import  guest_info_tool
+from alfred_agent.smolagent.tools import  guest_info_tool, search_tool, weather_info_tool, hub_stats_tool
 # Initialise the Hugging Face model
 model = InferenceClientModel()
 
 # Create Alfre,our gala agent, with guest info tool
-alfred = CodeAgent(tools=[guest_info_tool],model=model)
+alfred = CodeAgent(tools=[guest_info_tool,search_tool,weather_info_tool,hub_stats_tool],model=model)
 
 # Example query Alfred might receive during gala
 response = alfred.run("Tell me about our guest named 'Lady Ada Lovelace'.")
--- smolagent/tools.py
+++ smolagent/tools.py
@@ -1,6 +1,8 @@
-from smolagents import Tool
+import random
+from huggingface_hub import list_models
+from smolagents import Tool,DuckDuckGoSearchTool
 from langchain_community.retrievers import BM25Retriever
-from retrievers import docs
+from alfred_agent.smolagent.retrievers import docs
 class GuestInfoRetrieverTool(Tool):
     name = "GuestInfoRetrieverToll"
     description = "Retrieves guest information from a database"
@@ -23,4 +25,58 @@ class GuestInfoRetrieverTool(Tool):
         else:
             return "No matching guest information found"
         
+
+class WeatherInfoTool(Tool):
+    name = "WeatherInfoTool"
+    description = "Retrieves weather information for a given location"
+    inputs = {
+        "location": {
+            "type": "string",
+            "description": "The location for which you want the weather information"
+        }
+    }
+    output_type = "string"
+
+    def forward(self, location: str):
+        # Dummy weather data
+        weather_conditions = [
+            {"condition": "Rainy", "temp_c": 15},
+            {"condition": "Clear", "temp_c": 25},
+            {"condition": "Windy", "temp_c": 20}
+        ]
+        # Randomly select a weather condition
+        data = random.choice(weather_conditions)
+        return f"Weather in {location}: {data['condition']}, {data['temp_c']}°C"
+
+
+class HubStatsTool(Tool):
+    name = "HubStatsTool"
+    description = "Fetches the most dowmloaded models from Hugging Face Hub"
+    inputs = {
+        "author":{
+            "type": "string",
+            "description": "The author of the models you want to fetch"
+        }
+    }
+
+    output_type = "string"
+
+    def forward(self,author: str):
+        
+        try:
+            models = list(list_models(author=author, sort="downloads", direction=-1,limit=1))
+
+            if models:
+                model = models[0]
+                return f"Most downloaded model by {author}: {model.modelId} with {model.downloads} downloads"
+            else:
+                return f"No models found for author {author}"
+        except Exception as e:
+            return f"Error fetching models for author {author}: {str(e)}"
+    
+
+# Initialise the DuckDuckGo search tool and the guest info retriever tool
+search_tool = DuckDuckGoSearchTool()
 guest_info_tool = GuestInfoRetrieverTool(docs)
+weather_info_tool = WeatherInfoTool()
+hub_stats_tool = HubStatsTool()
